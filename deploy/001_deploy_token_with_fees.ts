0155c3cb1fee2c54232c930efd53596f0e3491dd
Aaron Lukacs <lukacsaronzs@gmail.com>
2023-05-02 09:37:09 +0300

    chore: deploy script

--- deploy/001_deploy_token_with_fees.ts
+++ deploy/001_deploy_token_with_fees.ts
@@ -10,29 +10,62 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
 	const { deployer } = await getNamedAccounts();
 
-	// TODO read from .env
-	// let args = [
-	// 	process.env.DEPLOY_DATA_NAME,
-	// 	process.env.DEPLOY_DATA_SYMBOL,
-	// 	process.env.DEPLOY_DATA_DECIMALS,
-	// 	process.env.DEPLOY_DATA_FEE_RATE,
-	// 	ethers.utils.parseUnits("0.1", 18),
-	// 	BigNumber.from(365 * 24 * 60 * 60),
-	// 	deployer,
-	// ]
+	let args;
 
-	const decimals = 8
 
-	let args = [
-		"Test Inflation Token",
-		"TIT",
-		decimals,
-		ethers.utils.parseUnits("0.01", decimals),
-		ethers.utils.parseUnits("0.1", decimals),
-		BigNumber.from(365 * 24 * 60 * 60),
-		deployer,
-		deployer,
-	]
+	if (hre.network.name !== "localhost" && hre.network.name !== "hardhat") {
+		if (!process.env.DEPLOY_DATA_NAME) {
+			throw new Error("Missing DEPLOY_DATA_NAME env variable")
+		}
+		if (!process.env.DEPLOY_DATA_SYMBOL) {
+			throw new Error("Missing DEPLOY_DATA_SYMBOL env variable")
+		}
+		if (!process.env.DEPLOY_DATA_DECIMALS) {
+			throw new Error("Missing DEPLOY_DATA_DECIMALS env variable")
+		}
+		if (!process.env.DEPLOY_DATA_FEE_RATE) {
+			throw new Error("Missing DEPLOY_DATA_FEE_RATE env variable")
+		}
+		if (!process.env.DEPLOY_DATA_MAX_FEE_RATE) {
+			throw new Error("Missing DEPLOY_DATA_MAX_FEE_RATE env variable")
+		}
+		if (!process.env.DEPLOY_DATA_MAX_DELAY_FEE_CHANGE) {
+			throw new Error("Missing DEPLOY_DATA_MAX_DELAY_FEE_CHANGE env variable")
+		}
+		if (!process.env.DEPLOY_DATA_FEE_COLLECTION_TREASURY_ADDRESS) {
+			throw new Error("Missing DEPLOY_DATA_FEE_COLLECTION_TREASURY_ADDRESS env variable")
+		}
+		if (!process.env.DEPLOY_DATA_MINTER_ADDRESS) {
+			throw new Error("Missing DEPLOY_DATA_MINTER_ADDRESS env variable")
+		}
+		args = [
+			process.env.DEPLOY_DATA_NAME,
+			process.env.DEPLOY_DATA_SYMBOL,
+			process.env.DEPLOY_DATA_DECIMALS,
+			ethers.utils.parseUnits(process.env.DEPLOY_DATA_FEE_RATE, 18),
+			ethers.utils.parseUnits(process.env.DEPLOY_DATA_MAX_FEE_RATE, 18),
+			BigNumber.from(process.env.DEPLOY_DATA_MAX_DELAY_FEE_CHANGE),
+			process.env.DEPLOY_DATA_FEE_COLLECTION_TREASURY_ADDRESS,
+			process.env.DEPLOY_DATA_MINTER_ADDRESS,
+		]
+
+	} else {
+		const decimals = 8
+
+		args = [
+			"Test Inflation Token",
+			"TIT",
+			decimals,
+			ethers.utils.parseUnits("0.01", decimals),
+			ethers.utils.parseUnits("0.1", decimals),
+			BigNumber.from(365 * 24 * 60 * 60),
+			deployer,
+			deployer,
+		]
+	}
+
+
+
 
 
 	const deployResult = await deploy('ERC20WithFees', {
