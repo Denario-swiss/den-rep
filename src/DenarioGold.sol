e5ae424f8968fc9001ec652d0cb89bb908712082
2075 <1140449+2075@users.noreply.github.com>
2024-10-03 14:44:13 +0200

    feat: add denario gold

---
+++ src/DenarioGold.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.27;
+
+import "./ERC20WithFeesUpgradeable.sol";
+
+import { UUPSUpgradeable } from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+
+contract DenarioGold is ERC20WithFeesUpgradeable, UUPSUpgradeable {
+	/// @custom:oz-upgrades-unsafe-allow constructor
+	constructor() {
+		_disableInitializers();
+	}
+
+	function initialize(
+		address _ownerAddress,
+		string memory _name,
+		string memory _symbol,
+		uint256 _fee,
+		uint256 _maxFee,
+		uint256 _delayFeeUpdate,
+		address _feeCollectionAddress,
+		address _minterAddress
+	) public initializer {
+		__ERC20WithFees_init(
+			_ownerAddress,
+			_name,
+			_symbol,
+			_fee,
+			_maxFee,
+			_delayFeeUpdate,
+			_feeCollectionAddress,
+			_minterAddress
+		);
+	}
+
+	function version() public pure returns (int8) {
+		return 1;
+	}
+
+	function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+}
---
+++ src/DenarioGoldV2.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+// This is a template contract for upgrading the original DSC contract
+
+pragma solidity ^0.8.27;
+
+import "./ERC20WithFeesUpgradeable.sol";
+
+import { UUPSUpgradeable } from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+
+contract DenarioGoldV2 is ERC20WithFeesUpgradeable, UUPSUpgradeable {
+	/// @custom:oz-upgrades-unsafe-allow constructor
+	constructor() {
+		_disableInitializers();
+	}
+
+	function version() public pure returns (int8) {
+		return 2;
+	}
+
+	function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+}
