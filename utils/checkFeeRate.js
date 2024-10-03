02e1c64bb6de2d1cf263283135ffd07aa2782b3c
2075 <1140449+2075@users.noreply.github.com>
2024-10-04 00:10:46 +0200

    feat: add reduce fee to upgrade contract

---
+++ utils/checkFeeRate.js
@@ -0,0 +1,27 @@
+async function main() {
+	const contractName = "DSCV2"
+	const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+	const myContract = await hre.ethers.getContractAt(
+		contractName,
+		contractAddress,
+	)
+
+	const version = await myContract.version()
+	console.log("current version:", version)
+
+	const feeRate = await myContract.feeRate()
+	console.log("current fee rate:", feeRate.toString())
+
+	const tx = await myContract.reduceFeeRate(0)
+	await tx.wait()
+
+	const newFeeRate = await myContract.feeRate()
+	console.log("new fee rate:", newFeeRate.toString())
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error)
+		process.exit(1)
+	})
